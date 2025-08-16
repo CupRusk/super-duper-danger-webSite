5c74bcb5e0d937f5e3ea165bac3339d40d180846
solevoy <csgofaceitden@gmail.com>
2025-08-16 21:16:22 +0300

    update js

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,1 +1,11 @@
-// Привет:) Да, я уже тебе подготовил файл)
+document.getElementById("block").addEventListener("submit", async (e) => {
+    e.preventDefault();
+    await fetch("http://localhost:3000/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            login: e.target.login.value,
+            password: e.target.password.value
+        })
+    });
+});
