6c9db3a725bd2a6a6c3a85728f353fa7ccdc7f1a
solevoy <csgofaceitden@gmail.com>
2025-08-16 22:00:19 +0300

    EZ SOLO FRONT-END

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,6 +1,6 @@
 document.getElementById("block").addEventListener("submit", async (e) => {
     e.preventDefault();
-    await fetch("http://localhost:8000/api/register", {
+    await fetch("http://localhost:8000/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
